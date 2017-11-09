699bd4824cd324b60cb43cc5076bf95cc5967546
Diego_wztao <diego_wztao@outlook.com>
2017-11-09 14:59:42 +0800

    Update oo.java

--- oo.java
+++ oo.java
@@ -6,21 +6,85 @@
      */
     public static String getXmlStringFromFile(String filename) {
         try {
-            File file = new File(filename);
-            FileInputStream fis = new FileInputStream(file);
-            //            fis.reset();
-            byte[] b = new byte[(int) file.length()];
-            fis.read(b);
+        File f = new File("c:/po.xml");
+        // counting child elements of parlist
+	int count = 0;
+        // counting child elements of parlist named "par"
+	int par_count = 0;
+	FileInputStream fis =  new FileInputStream(f);
+        byte[] b = new byte[(int) f.length()];
+	fis.read(b);
+	VTDGen vg = new VTDGen();
+	vg.setDoc(b);
+	vg.parse(true);
+	VTDNav vn = vg.getNav();
+        if (vn.matchElement("bix")){ // match blix
+     	  // to first child named "package"
+          if (vn.toElement(VTDNav.FC,"package")){ 
+	    do {
+		    System.out.println("package");
+		    // to first child named "command"
+	      if (vn.toElement(VTDNav.FC,"command"))
+	      {
+	 	 do {
+		 	System.out.println("command");
+ 			if (vn.toElement(VTDNav.FC, "parlist")){
+	   		   do {
+				System.out.println("parlist");
+				if (vn.toElement(VTDNav.FC)){
+			        do {	
+				      count++; //increment count
+				      if (vn.matchElement("par"))
+					 par_count++;
+			   	}
+				while(vn.toElement(VTDNav.NS));
+				vn.toElement(VTDNav.P);
+			     }
+			}
+			while (vn.toElement(VTDNav.NS,"parlist"));
+			vn.toElement(VTDNav.P);
+		    }						
+	     }
+	     // to next silbing named "command"
+	     while (vn.toElement(VTDNav.NS, "command")); 
+	     vn.toElement(VTDNav.P); // go up one level
+	    }
+	    else 
+	      System.out.println(" no child element named 'command' ");
+	    // verify result
 
-            String msg = new String(b, "utf-8");
+	  }
+	  while(vn.toElement(VTDNav.NS,"package")); // to next sibling named "package"
+	  vn.toElement(VTDNav.P); // go up one level	
+	 }else
+	    System.out.println(" no child element named 'package' ");	
+	} else 
+	  System.out.println(" Root is not 'bix' ");
+	// print out the results
+	System.out.println(" count ====> " + count);
+	System.out.println(" par_count ==> " + par_count);
 
-            fis.close();
-            if (logger.isInfoEnabled()) {
-                logger.info("get file success:" + filename);
-            }
-            return msg;
-        } catch (Exception ex) {
-            logger.info(ex.getMessage());
-            return null;
-        }
+	// verify results using iterators
+        int v=0;
+        vn.toElement(VTDNav.ROOT);
+        AutoPilot ap = new AutoPilot(vn);
+        ap.selectElement("par");
+        while(ap.iterate()){
+    	   if (vn.getCurrentDepth() == 4) 
+	   {
+		v++;
+	   }
+	}
+	System.out.println(" verify ==> "+v);
+     }
+     catch (ParseException e){
+	     System.out.println(" XML file parsing error \n"+e);
+     }
+     catch (NavException e){
+	     System.out.println(" Exception during navigation "+e);
+     }
+     catch (java.io.IOException e)
+     {
+	     System.out.println(" IO exception condition"+e);
+     }
     }
